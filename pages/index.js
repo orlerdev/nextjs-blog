ae7200623c900b59edb638cef949b88f4b1e0436
Jared Orler <orlerdev@icloud.com>
2023-05-03 15:07:27 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,50 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from "../utils/posts";
+import Link from 'next/link';
+import Date from '../components/date';
+
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData();
+    return {
+        props: {
+            allPostsData
+        }
+    };
+}
+
+export default function Home({allPostsData}) {
+    return (
+        <Layout home>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({id, date, title}) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            <Link href={`/posts/${id}`}>{title}</Link>
+                            <br />
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date} />
+                            </small>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+            <section className={utilStyles.headingMd}>
+                <p>Proud military veteran and Full Stack Software Developer with exceptional leadership skills.
+                    Experience in overseeing maintenance, personnel evaluations, and overhauling procedures.
+                    Successfully developed training programs, facilitated high-risk training evolutions, and corrected
+                    deficiencies in damage control curriculum as Warfare Manager and Instructor. Ready to leverage
+                    leadership expertise as a software developer.</p>
+                <p>
+                    (This is a sample website - you’ll be building a site like this on{' '}
+                    <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+                </p>
+            </section>
+        </Layout>
+    );
+}
